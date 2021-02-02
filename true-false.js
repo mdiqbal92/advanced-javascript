1769f127074b68fcc18215f2ff77fd41db4844b4
Md Ibal Hossain <iqbal.h1992@gmail.com>
2021-02-02 17:38:17 +0600

    true/false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+//true values
+// '0'
+// " "
+// []
+
+//False values
+// 0
+// ""
+//false itself
+// undefined
+// Nan
+//null
+
+let name = "";
+console.log(name);
+if(name){
+console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
