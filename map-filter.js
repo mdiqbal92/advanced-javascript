a1c46d86992dbc3c5ad0a0668fcc2543f84a3476
Md Ibal Hossain <iqbal.h1992@gmail.com>
2021-02-02 23:43:41 +0600

    map-filter

---
+++ map-filter.js
@@ -0,0 +1,6 @@
+const numbers = [3, 5, 7,8,9,15,26,32];
+// const squire = numbers.map(x => x * x);
+const bigger = numbers.filter(x => x > 5);
+// const squire = numbers.find(x => x > 5);
+console.log(bigger);
+
